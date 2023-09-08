2568d048c04cb756d89cbfcd9b501e5a435c6b3f
Omoluabi <emmanuelogunleye441999@gmail.com>
2023-09-08 12:28:26 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+  * main - main function
+  * Return: always 0 (success)
+  */
+int main(void)
+{
+	printf("with proper gramma, but the outcome is a piece of art,");
+	return (0);
+}
