abcea6188fa41a1b5f9dc6ae2c6bc5dd627c61a8
Omoluabi <emmanuelogunleye441999@gmail.com>
2023-09-11 21:40:06 +0000

    hhh

---
+++ 0x00-hello_world/101-qoute.c
@@ -0,0 +1,15 @@
+#include <unistd.h>
+
+/**
+ * main - this prints exactly the input on i
+ * Return: always 1.
+ */
+
+int main(void)
+
+8{
+	write(2, "and that piece of art is useful\" - DoraKorpar, 2015-10-19\n", 59);
+
+	return (1);
+
+}
