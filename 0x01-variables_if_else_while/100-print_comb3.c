7f37439de3254e4c917871df8a54b69994726da2
Omoluabi <emmanuelogunleye441999@gmail.com>
2023-09-11 17:53:07 +0000

    printed two character saperated comma and whitespace[H

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+ *main - printing two character  separted by , and space
+ *Return: return 0 if succeeed
+ */
+
+int main(void)
+{
+	int i = '0';
+	int j = '1';
+
+	while (i <= '9')
+	{
+		while(j  <= '9')
+		{
+			putchar(i);
+			putchar(j);
+		        j++;
+			if (i < '8' || j < '9')
+			  {
+			 	putchar(',');
+			 	putchar(' ');
+			  }
+		}
+		i++;
+		j = i + 1;
+	}
+	putchar('\n');
+	return (0);
+}
