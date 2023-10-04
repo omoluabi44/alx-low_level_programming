fd0b43e86bced323a2f2af7c344193b751e49fb8
Omoluabi <emmanuelogunleye441999@gmail.com>
2023-10-04 11:50:00 +0000

    correct the bugs

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -13,9 +13,13 @@ char *str_concat(char *s1, char *s2)
 	int j;
 	char *concat;
 
-	if ((s1 == NULL) || (s2 == NULL))
+	if (s1 == NULL)
 	{
 		s1 = " ";
+	}
+
+	if (s2 == NULL)
+	{
 		s2 = " ";
 	}
 
