93a7f71cd20cf8e837eec2c36790324d75c3d90f
Omoluabi <emmanuelogunleye441999@gmail.com>
2023-10-04 09:48:07 +0000

    created array of char and initialize to c

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+ * *create_array - function create array of specific and initialise with c.
+ *@size: size of the array
+ *@c: initialize the array with c.
+ *Return: create_array.
+ */
+char *create_array(unsigned int size, char c)
+{
+	unsigned int i;
+	char *create_array = malloc(size * sizeof(char));
+
+	if ((size == 0) && (create_array == NULL))
+	{
+		return (NULL);
+	}
+
+	/* if (create_array == NULL) */
+	/* { */
+	/*return (NULL); */
+	/* } */
+
+	for (i = 0; i < size; i++)
+	{
+		create_array[i] = c;
+	}
+	return (create_array);
+}
---
+++ 0x0B-malloc_free/main.h
@@ -0,0 +1,5 @@
+#ifndef MAIN_H
+#define MAIH_H
+int _putchar(char c);
+char *create_array(unsigned int size, char c);
+#endif
