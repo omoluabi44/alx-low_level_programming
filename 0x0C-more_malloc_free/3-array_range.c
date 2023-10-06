da29efe279c23f32ef6416a61091184eac41b88d
Omoluabi <emmanuelogunleye441999@gmail.com>
2023-10-06 13:58:38 +0000

    aarray of function

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+ *array_range - sub min from max and return min
+ *@min: param
+ *@max: param
+ *Return: aray
+ */
+int *array_range(int min, int max)
+{
+	int length, i;
+	int *aray;
+
+	if (min > max)
+	{
+		return (NULL);
+	}
+
+	length = max - min + 1;
+
+	aray  = malloc(sizeof(int) * length);
+	if (aray == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; i < length; i++)
+	{
+		aray[i] = min + 1;
+	}
+
+	return (aray);
+}
