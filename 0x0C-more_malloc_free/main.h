1e4d8fd2d3c8442fa5827653eff0c181b20f1fa7
Omoluabi <emmanuelogunleye441999@gmail.com>
2023-10-08 20:38:10 +0000

    done

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,5 +1,10 @@
 #ifndef MAIN_H
 #define MAIH_H
+int main(int argc, char *argv[]);
+
+void errors();
+int is_digit (char *dig);
+int _strlen(char *length);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 int *array_range(int min, int max);
 void *_calloc(unsigned int nmemb, unsigned int size);
