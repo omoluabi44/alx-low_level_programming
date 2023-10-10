d4f6d9888ea230e2205401db48f0dbed0d830f19
Omoluabi <emmanuelogunleye441999@gmail.com>
2023-10-10 22:56:57 +0000

    struct for dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ *struct dog - structure for dogs
+ *@name: name of the dog
+ *@owner:owner of the dog
+ *@age: age of the dog
+ *
+ *Description: this structure crate details of each dog in the hospital
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
