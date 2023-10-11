f526d7fea13505da0cc73a2354e79a6935e66df7
Omoluabi <emmanuelogunleye441999@gmail.com>
2023-10-11 18:55:39 +0000

    null

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,3 +1,4 @@
+#include <stdlib.h>
 /**
  * print_name - function to pointers that call function
  *@name: value passed
@@ -7,7 +8,7 @@
 
 void print_name(char *name, void (*f)(char *))
 {
-	if (name == "0")
+	if (name == NULL)
 		return;
 	f(name);
 }
