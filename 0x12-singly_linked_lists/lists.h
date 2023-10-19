6b82a2c9f48f72115bbc39c8d8d02050e863b757
Omoluabi <emmanuelogunleye441999@gmail.com>
2023-10-19 17:06:12 +0000

    betty style

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,10 +1,8 @@
 #ifndef _LIST
 #define _LIST
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
 /**
  * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
@@ -15,10 +13,9 @@
  */
 typedef struct list_s
 {
-    char *str;
-    unsigned int len;
-    struct list_s *next;
+	char *str;
+	unsigned int len;
+	struct list_s *next;
 } list_t;
 size_t print_list(const list_t *h);
-
 #endif
