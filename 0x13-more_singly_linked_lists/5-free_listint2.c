f3c93a0e0ad8f0bbce6338a1e0902b73b8826e54
Omoluabi <emmanuelogunleye441999@gmail.com>
2023-10-23 19:08:23 +0000

    free node

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,15 +1,16 @@
 #include "lists.h"
 /**
- *free_listint - free each node in element
+ *free_listint2 - free each node in element
  *@head: head pointr
  */
 
 void free_listint2(listint_t **head)
 {
 	listint_t *tmp;
-	if (head == NULL || *head == NULL)
+
+	if ((head == NULL) || (*head == NULL))
 		return;
-	while ((*head)->next !=NULL)
+	while ((*head)->next != NULL)
 	{
 		tmp = (*head)->next;
 		free(*head);
