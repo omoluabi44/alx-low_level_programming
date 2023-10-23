7ea70d980dcc349fd9fa040ceb67793f854901e2
Omoluabi <emmanuelogunleye441999@gmail.com>
2023-10-23 17:16:56 +0000

    bug fixed

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -13,7 +13,7 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	{
 		return (0);
 	}
-	newNode = (listint_t *)malloc(sizeof(listint_t));
+	newNode = malloc(sizeof(listint_t));
 	if (newNode == NULL)
 	{
 		return (0);
