730ee79254e113be5f903bd3274b4c56c9010ed2
Omoluabi <emmanuelogunleye441999@gmail.com>
2023-10-23 18:19:28 +0000

    finall

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -24,8 +24,8 @@ listint_t *add_nodeint(listint_t **head, const int n)
 		newNode->next = NULL;
 	}
 	else
-		newNode->n = n;
-	newNode->next = *head;
+		newNode->next = *head;
+	newNode->n = n;
 	*head = newNode;
 	return (*head);
 }
