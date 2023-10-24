c4b3a0154920174203ce2856a7fd659992975131
Omoluabi <emmanuelogunleye441999@gmail.com>
2023-10-24 14:04:12 +0000

    betty style

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -21,7 +21,7 @@ size_t listint_len(const listint_t *h);
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index);
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
 size_t print_listint(const listint_t *h);
-listint_t *new_node( int n);
+listint_t *new_node(int n);
 int pop_listint(listint_t **head);
 void free_listint2(listint_t **head);
 void free_listint(listint_t *head);
