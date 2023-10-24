da0decf09b06211246f141e23843900ab06aede8
Omoluabi <emmanuelogunleye441999@gmail.com>
2023-10-24 07:49:16 +0000

    get index of given number in singly linked list

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+/**
+ *get_nodeint_at_index - get the specified given position in singly linked list
+ *@head: the head of the singly linked list
+ *@index: index of given position in a singly linked list
+ *Return: null if index is out of range or head if within range
+ */
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	unsigned int keepCount = 0;
+
+	while (keepCount < index )
+	{
+		if (head == NULL)
+			return (NULL);
+		head = head->next;
+		keepCount++;
+	}
+	return (head);
+	
+}
