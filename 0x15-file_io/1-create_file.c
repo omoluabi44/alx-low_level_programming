f90f860e82abc136761ebd83a9e9edef9ca55262
omoluabi44 <emmanuelogunleye441999@gmail.com>
2023-10-30 19:54:25 +0100

    corrected output

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,15 +13,19 @@ int create_file(const char *filename, char *text_content)
 
 	if (!filename)
 		return (-1);
+
 	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
 	if (fd == -1)
-		return (0);
+		return (-1);
+
 	if (text_content == NULL)
 		text_content = "";
+
 	for (letterN = 0; text_content[letterN]; letterN++)
 
 	nwrite = write(fd, text_content, letterN);
+
 	if (nwrite == -1)
 		return (-1);
 
