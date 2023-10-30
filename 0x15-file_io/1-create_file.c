c23686f80bb87952d6f65e287ea67331dbda8915
omoluabi44 <emmanuelogunleye441999@gmail.com>
2023-10-30 19:57:03 +0100

    betty style

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -22,8 +22,8 @@ int create_file(const char *filename, char *text_content)
 	if (text_content == NULL)
 		text_content = "";
 
-	for (letterN = 0; text_content[letterN]; letterN++);
-
+	for (letterN = 0; text_content[letterN]; letterN++)
+		;
 	nwrite = write(fd, text_content, letterN);
 
 	if (nwrite == -1)
