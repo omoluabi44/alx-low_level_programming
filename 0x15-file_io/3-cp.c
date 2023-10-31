f94cef5dff15c96a679050b8171a509999b0157b
omoluabi44 <emmanuelogunleye441999@gmail.com>
2023-10-31 17:07:21 +0100

    fixing

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,12 +1,83 @@
-#include "main.h"
-/**
- * error - check if a file is opened  and handle errors
- *file_from: initial file to copy from
- *file_to: file destination
- *Return: nothing
- */
+/* #include "main.h" */
+/* /\** */
+/*  * error - check if a file is opened  and handle errors */
+/*  *file_from: initial file to copy from */
+/*  *file_to: file destination */
+/*  *Return: nothing */
+/*  *\/ */
 
-void error_(int file_from, int file_to, char *argv[])
+/* void error_(int file_from, int file_to, char *argv[]) */
+/* { */
+/* 	if (file_from == -1) */
+/* 	{ */
+/* 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]); */
+/* 		exit(98); */
+/* 	} */
+/* 	if (file_to == -1) */
+/*         { */
+/*                 dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", argv[2]); */
+/* 		exit(99); */
+/*         } */
+/* } */
+
+/* /\** */
+/*  * main - copy content of a file from one to another */
+/*  *@argc: arguement count */
+/*  *@argv: arguement paramter */
+/*  Return: 0 on success */
+/* *\/ */
+/* int main (int argc, char *argv[]) */
+/* { */
+/* 	int file_from, file_to, error_c; */
+/* 	ssize_t nchars, nw; */
+/* 	char buf[1024]; */
+
+/* 	if (argc != 3) */
+/* 	{ */
+/* 		dprintf(STDERR_FILENO, "%s\n", "Usage: cp file_from file_to"); */
+/* 		exit(97); */
+/* 	} */
+
+/* 	file_from = open(argv[1], O_RDONLY); */
+/* 	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664); */
+/* 	error_(file_from, file_to, argv); */
+
+/* 	nchars = 1024; */
+/* 	while (nchars == 1024) */
+/* 	{ */
+/* 		nchars = read(file_from, buf, 1024); */
+/* 		if (nchars == -1) */
+/* 			error_(-1, 0, argv); */
+
+/* 		nw = write(file_to, buf, nchars); */
+/* 		if (nw == -1) */
+/* 			error_(0, -1, argv); */
+/* 	} */
+/* 	error_c = close(file_from); */
+/* 	if (error_c == -1) */
+/* 	{ */
+/* 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from); */
+/* 		exit(100); */
+/* 	} */
+/* 	error_c = close(file_to); */
+/* 	if (error_c == -1) */
+/*         { */
+/*                 dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_to); */
+/*                 exit(100); */
+/*         } */
+/* 	return (0); */
+/* } */
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * error_file - checks if files can be opened.
+ * @file_from: file_from.
+ * @file_to: file_to.
+ * @argv: arguments vector.
+ * Return: no return.
+ */
+void error_file(int file_from, int file_to, char *argv[])
 {
 	if (file_from == -1)
 	{
@@ -14,22 +85,22 @@ void error_(int file_from, int file_to, char *argv[])
 		exit(98);
 	}
 	if (file_to == -1)
-        {
-                dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", argv[2]);
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 		exit(99);
-        }
+	}
 }
 
 /**
- * main - copy content of a file from one to another
- *@argc: arguement count
- *@argv: arguement paramter
- Return: 0 on success
-*/
-int main (int argc, char *argv[])
+ * main - check the code for Holberton School students.
+ * @argc: number of arguments.
+ * @argv: arguments vector.
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
 {
-	int file_from, file_to, error_c;
-	ssize_t nchars, nw;
+	int file_from, file_to, err_close;
+	ssize_t nchars, nwr;
 	char buf[1024];
 
 	if (argc != 3)
@@ -40,30 +111,31 @@ int main (int argc, char *argv[])
 
 	file_from = open(argv[1], O_RDONLY);
 	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
-	error_(file_from, file_to, argv);
+	error_file(file_from, file_to, argv);
 
 	nchars = 1024;
 	while (nchars == 1024)
 	{
 		nchars = read(file_from, buf, 1024);
 		if (nchars == -1)
-			error_(-1, 0, argv);
-
-		nw = write(file_to, buf, nchars);
-		if (nw == -1)
-			error_(0, -1, argv);
+			error_file(-1, 0, argv);
+		nwr = write(file_to, buf, nchars);
+		if (nwr == -1)
+			error_file(0, -1, argv);
 	}
-	error_c = close(file_from);
-	if (error_c == -1)
+
+	err_close = close(file_from);
+	if (err_close == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
 		exit(100);
 	}
-	error_c = close(file_to);
-	if (error_c == -1)
-        {
-                dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_to);
-                exit(100);
-        }
+
+	err_close = close(file_to);
+	if (err_close == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
+		exit(100);
+	}
 	return (0);
 }
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -7,7 +7,10 @@
 #include<sys/stat.h>
 #include <fcntl.h>
 /* #include<fcontl.h>  */
+
+int main (int argc, char *argv[]);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
+void error_file(int file_from, int file_to, char *argv[]);
 int append_text_to_file(const char *filename, char *text_content);
 #endif
