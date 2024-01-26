375b12aa76c5faa9e68f6e46eb28c02c6b9f0e58
omoluabi44 <emmanuelogunleye441999@gmail.com>
2024-01-26 13:07:36 +0100

    betty style

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -15,7 +15,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 
 	if (idx == 0)
 	{
-		return(add_dnodeint(h, n));
+		return (add_dnodeint(h, n));
 	}
 	while (idx != 1)
 	{
