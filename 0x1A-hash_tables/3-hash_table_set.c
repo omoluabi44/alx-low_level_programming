ef4714e00f1a6682f21564d97b54d95e4d0edc93
omoluabi44 <emmanuelogunleye441999@gmail.com>
2024-02-16 10:48:50 +0100

    added item to table

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,42 @@
+#include "hash_tables.h"
+/**
+ * hash_table_set - add element to the hash table
+ *@ht: the table to modified
+ *@key: the key
+ *@value: the value to be added to the table
+ *Return: the 1 if success, 0 otherwise
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	/**#get the key hash function*/
+	unsigned long int new_key;
+	hash_node_t *new_node;
+
+
+	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
+		return (0);
+
+	new_key = key_index((unsigned char *)key, ht->size);
+	new_node = malloc(sizeof(hash_node_t));
+	if (new_node == NULL)
+		return (0);
+
+	new_node->value = strdup(value);
+	if (new_node->value == NULL)
+	{
+		free(new_node);
+		return (0);
+	}
+	new_node->key = strdup(key);
+	if (new_node->key == NULL)
+	{
+		free(new_node);
+		free(new_node->value);
+		return (0);
+	}
+
+	new_node->next = ht->array[new_key];
+	ht->array[new_key] = new_node;
+
+	return (1);
+}
