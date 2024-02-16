d89724e8f1c144290b006d696f7f0ec080a59cdb
omoluabi44 <emmanuelogunleye441999@gmail.com>
2024-02-16 18:11:46 +0100

    task 6

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,35 @@
+#include "hash_tables.h"
+/**
+ *hash_table_delete - print all elemeent in hash table
+ *@ht: the table
+ *Return: nothing
+ */
+void hash_table_delete(hash_table_t *ht)
+{
+	unsigned long int i;
+	hash_node_t *current;
+	hash_table_t *head = ht;
+	hash_node_t *next_node;
+
+
+	if (ht == NULL)
+	{
+		return;
+	}
+
+	for (i = 0; i < ht->size; i++)
+	{
+		current = ht->array[i];
+
+		while (current != NULL)
+		{
+			next_node = current->next;
+			free(current->key);
+			free(current->value);
+			free(current);
+			current = next_node;
+		}
+	}
+	free(head->array);
+	free(head);
+}
