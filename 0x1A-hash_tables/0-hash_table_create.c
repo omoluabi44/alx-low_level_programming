9fafd7fc005194a0c215c1054ed0ef93b6c5661e
omoluabi44 <emmanuelogunleye441999@gmail.com>
2024-02-16 08:16:58 +0100

    created hash table

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,32 @@
+#include "hash_tables.h"
+/**
+ *hash_table_create - creating a new hash Table
+ *@size: size  of the table
+ *Return: address of the new created hash table
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *Table;
+	unsigned long int i;
+
+	Table = malloc(sizeof(hash_table_t));
+
+	if (Table == NULL)
+	{
+		return (NULL);
+	}
+
+	Table->array = malloc(sizeof(hash_table_t *) * size);
+	if (Table->array == NULL)
+	{
+		free(Table);
+		return (NULL);
+	}
+	Table->size = size;
+
+	for (i = 0; i < size; i++)
+	{
+		Table->array[i] = NULL;
+	}
+	return (Table);
+}
