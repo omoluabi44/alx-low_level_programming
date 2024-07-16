11ab387651e2e8f07806e9bf22c5eb1a5d0b20a5
omoluabi44 <emmanuelogunleye441999@gmail.com>
2024-07-16 16:19:35 +0100

    task 1

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "search_algos.h"
+
+/**
+ *linear_search - search for element in array using linear search
+ *@array: the array
+ *@size: size of the array
+ *@value: the target
+ *Return: index of the element if found else return -1
+ */
+
+int linear_search(int *array, size_t size, int value)
+{
+	size_t i;
+
+	for (i = 0; i < size; i++)
+	{
+		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
+		if (array[i] == value)
+		{
+			return (i);
+		}
+	}
+	return (-1);
+}
---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,5 @@
+#ifndef MAIN_H
+#define MAIH_H
+int linear_search(int *array, size_t size, int value);
+int main(void);
+#endif
