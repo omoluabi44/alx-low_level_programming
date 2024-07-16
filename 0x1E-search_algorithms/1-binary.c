5cabd01f2ebbaf60adb5cfa1d006098e48cde440
omoluabi44 <emmanuelogunleye441999@gmail.com>
2024-07-16 17:39:43 +0100

    task 2

---
+++ 0x1E-search_algorithms/1-binary.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include "search_algos.h"
+/**
+ *binary_search - search for element in array using linear search
+ *@array: the array
+ *@size: size of the array
+ *@value: the target
+ *Return: index of the element if found else return -1
+ */
+int binary_search(int *array, size_t size, int value)
+{
+	size_t left, right, mid, i;
+
+	left = 0;
+	right = size - 1;
+
+	while (left <= right)
+	{
+		mid = left + (right - left) / 2;
+		printf("Searching in array : ");
+		for (i = left; i <= right; i++)
+		{
+			if (i > left)
+			{
+				printf(", ");
+			}
+			printf("%lu", i);
+		}
+		printf("\n");
+		if (value == array[mid])
+			return (mid);
+		else if (value < array[mid])
+		{
+			right = mid - 1;
+		}
+		else
+		{
+			left = mid + 1;
+		}
+	}
+	return (-1);
+}
